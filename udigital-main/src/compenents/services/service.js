3e5d36b4955c650ada7688b1e2a65dfede410abe
ZAROUR Ayoub <109147926+Ayz1325@users.noreply.github.com>
2022-12-10 19:23:04 +0100

    Add files via upload

---
+++ udigital-main/src/compenents/services/service.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './service.css'
+import Ser from './serv'
+
+function Service()
+{
+    
+    return (
+        <div className='conS'>
+            <div className='top'>
+               <h2>Our Services</h2>
+               <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
+            </div>
+            <div className='bottom'>
+              <Ser />
+            </div>
+           
+    
+        </div>
+    )
+}
+
+export default Service
